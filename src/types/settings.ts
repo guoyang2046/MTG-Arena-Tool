1ee722878a0fea6f496327dc2fdd9e9116de2677
Benjamin Lu <lu.s.benjamin@gmail.com>
2020-01-27 21:53:36 -0800

    Update settings.ts

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,3 +1,10 @@
+import { TableState } from "react-table";
+import { CardsData } from "../window_main/components/collection/types";
+import { DecksData } from "../window_main/components/decks/types";
+import { TransactionData } from "../window_main/components/economy/types";
+import { EventTableData } from "../window_main/components/events/types";
+import { MatchTableData } from "../window_main/components/matches/types";
+
 export interface OverlaySettingsData {
   alpha: number;
   alpha_back: number;
@@ -19,18 +26,52 @@ export interface OverlaySettingsData {
   type_counts: boolean;
 }
 
+export interface AppSettings {
+  auto_login: boolean;
+  beta_channel: boolean;
+  email: string;
+  launch_to_tray: boolean;
+  log_locale_format: string;
+  logUri: string;
+  metadata_lang: string;
+  remember_me: boolean;
+  token: string;
+  toolVersion: number;
+}
+
 export interface SettingsData {
-  sound_priority: boolean;
-  sound_priority_volume: number;
-  cards_quality: string;
-  cards_size: number;
-  cards_size_hover_card: number;
-  overlay_back_color: string;
+  anon_explore: boolean;
   back_color: string;
   back_url: string;
   card_tile_style: number | string;
+  cards_quality: string;
+  cards_size: number;
+  cards_size_hover_card: number;
+  close_on_match: boolean;
+  collectionTableState?: TableState<CardsData>;
+  collectionTableMode: string;
+  decksTableMode: string;
+  decksTableState?: TableState<DecksData>;
+  economyTableMode: string;
+  economyTableState?: TableState<TransactionData>;
+  eventsTableMode: string;
+  eventsTableState?: TableState<EventTableData>;
+  export_format: string;
+  last_date_filter: string;
+  last_open_tab: number;
+  matchesTableMode: string;
+  matchesTableState?: TableState<MatchTableData>;
+  metadata_lang: string;
+  overlay_back_color: string;
   overlay_scale: number;
-  overlays: OverlaySettingsData[];
   overlayHover: { x: number; y: number };
+  overlays: OverlaySettingsData[];
+  right_panel_width: number;
+  skip_firstpass: boolean;
+  sound_priority: boolean;
+  sound_priority_volume: number;
   shortcut_editmode: string;
+  send_data: boolean;
 }
+
+export interface MergedSettings extends AppSettings, SettingsData {}
